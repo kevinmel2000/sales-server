4c16dc339cff63542e4b8029896f4bc3917518aa
Tokang Developer <tokang.dev@gmail.com>
2019-05-21 12:46:21 +0700

    add authorization

---
+++ src/middleware/check-auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+/**
+ * Add token validation from header
+ */
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userToken = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Token is missing or wrong or already expired. Please generate a new one and put it inside Authorization key in header'
+    });
+  }    
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 
 const ProductController = require('../controllers/product');
 
 // Auth Route
-router.get('/', ProductController.listProducts);
+router.get('/', checkAuth, ProductController.listProducts);
 
 
 module.exports = router;
